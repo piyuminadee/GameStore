7d7fe5029332c0abab63a1ba6a63a2640f6381ea
Nadeesh Weerarathna <nadeesh-cs20024@stu.kln.ac.lk>
2025-02-16 20:22:49 +0530

    Get and Post Request

--- GameStore.Api/Entities/Game.cs
+++ GameStore.Api/Entities/Game.cs
@@ -3,6 +3,14 @@ namespace GameStore.Api.Entities
     public class Game
     {
        public int Id { get; set; } 
-       public string? Name { get; set; }
+       public required string Name { get; set; }
+
+       public DateTime ReleaseData { get; set; }
+    
+       public required string Genre { get; set; }
+
+       public decimal Price { get; set; }
+
+       public required string ImageUri { get; set; }
     }
 }
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,60 @@
+using GameStore.Api.Entities;
+const string GetGameEndPoint = "GetGame";
+
+List<Game> games = new()
+{
+    new Game()
+    {
+          Genre = "Cars",
+          Id= 1,
+          Name = "Need For Speed",
+          Price = 19.45M,
+          ReleaseData = new DateTime(1890, 2 ,1),
+          ImageUri = "https://placehold.co/100"
+    },
+
+    new Game()
+    {
+          Id= 2,
+          Genre = "Action",
+          Name = "Temple Run",
+          Price = 9.65M,
+          ReleaseData = new DateTime(1994, 5 ,3),
+          ImageUri = "https://placehold.co/100"
+    },
+    new Game()
+    {
+          Id= 3,
+          Genre = "Action",
+          Name = "YCT",
+          Price = 59.65M,
+          ReleaseData = new DateTime(2010, 2 ,8),
+          ImageUri = "https://placehold.co/100"
+    },
+};
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/games", () => games);
+app.MapGet("/games/{id}", (int id) =>
+ {
+      Game? game = games.Find(game => game.Id == id);
+      if (game is null)
+      {
+            return Results.NotFound();
+      }
+
+      return Results.Ok(game);
+ })
+ .WithName(GetGameEndPoint);
+
+app.MapPost("/games", (Game game) =>
+{
+      game.Id = games.Max(game => game.Id) + 1;
+      games.Add(game);
+
+      return Results.CreatedAtRoute(GetGameEndPoint, new {id = game.Id}, game);
+});
 
 app.Run();
