a543fed76f98f0e6432eaa18fe0124ab61cf3c1c
Nadeesh Weerarathna <nadeesh-cs20024@stu.kln.ac.lk>
2025-02-23 20:51:32 +0530

    put request

--- GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a65b88039fe72b229947acdeb96ba6e7caccd4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d7fe5029332c0abab63a1ba6a63a2640f6381ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
