ea33e3971a010ef68511a296f5e69f1d71e551e0
Nadeesh Weerarathna <nadeesh-cs20024@stu.kln.ac.lk>
2025-02-25 09:19:23 +0530

    group endpoints using route group builder

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,93 +1,10 @@
 using GameStore.Api.Entities;
-const string GetGameEndPoint = "GetGame";
 
-List<Game> games = new()
-{
-    new Game()
-    {
-          Genre = "Cars",
-          Id= 1,
-          Name = "Need For Speed",
-          Price = 19.45M,
-          ReleaseData = new DateTime(1890, 2 ,1),
-          ImageUri = "https://placehold.co/100"
-    },
 
-    new Game()
-    {
-          Id= 2,
-          Genre = "Action",
-          Name = "Temple Run",
-          Price = 9.65M,
-          ReleaseData = new DateTime(1994, 5 ,3),
-          ImageUri = "https://placehold.co/100"
-    },
-    new Game()
-    {
-          Id= 3,
-          Genre = "Action",
-          Name = "YCT",
-          Price = 59.65M,
-          ReleaseData = new DateTime(2010, 2 ,8),
-          ImageUri = "https://placehold.co/100"
-    },
-};
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
-var group = app.MapGroup("/games");
-
-app.MapGet("/", () => games);
-app.MapGet("/{id}", (int id) =>
- {
-      Game? game = games.Find(game => game.Id == id);
-      if (game is null)
-      {
-            return Results.NotFound();
-      }
-
-      return Results.Ok(game);
- })
- .WithName(GetGameEndPoint);
-
-app.MapPost("/", (Game game) =>
-{
-      game.Id = games.Max(game => game.Id) + 1;
-      games.Add(game);
-
-      return Results.CreatedAtRoute(GetGameEndPoint, new {id = game.Id}, game);
-});
-
-app.MapPut("/{id}", (int id, Game updatedGame ) =>
- {
-      Game? existingGame = games.Find(game => game.Id == id);
-      if (existingGame is null)
-      {
-            return Results.NotFound();
-      }
-
-      existingGame.Genre = updatedGame.Genre;
-      existingGame.Name = updatedGame.Name;
-      existingGame.Price = updatedGame.Price;
-      existingGame.ReleaseData = updatedGame.ReleaseData;
-      existingGame.ImageUri = updatedGame.ImageUri;
-      
-
-      return Results.NoContent();
- });
-
- app.MapDelete("/{id}", (int id) =>
- {
-      Game? game = games.Find(game => game.Id == id);
-      if (game is not null)
-      {
-            games.Remove(game);
-      }
-
-      return Results.NoContent();
- });
- 
+             
 
 
 app.Run();
--- GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a543fed76f98f0e6432eaa18fe0124ab61cf3c1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07330531a5297a841649cc762fd343002ba15078")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
